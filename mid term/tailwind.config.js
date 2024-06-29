58d2c8260ec4cecf057cd590869e7baa08ee8daf
Asad <asadullah.jan@outlook.com>
2024-06-29 12:59:09 +0500

    feat/ added tailwind.css

---
+++ mid term/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs", "./public/**/*.css"], // Adjust paths based on your actual directory structure
+  theme: {
+    extend: {}, // Optional: Extend Tailwind's default theme
+  },
+  plugins: [], // Optional: Add Tailwind plugins
+};
